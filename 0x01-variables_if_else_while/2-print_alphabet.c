f929f3f1f530f349fc291c4026ed6d7629d1362e
Wanambisix <sultanmzuri@gmail.com>
2022-09-09 11:23:24 -0700

    I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,22 +1,21 @@
 #include <stdio.h>
-
 /**
- * main - Prints the alphabet in lowercase, and then in uppercase.
- *
- * Return: Always 0.
+ * main - printing numbers from 0-9 with commas and space between them
+ * Description: using the main function
+ * this program prints "0, 1, 2, 3, 4, 5, 6, 7, 8, 9"
+ * Return: 0
  */
 int main(void)
 {
-	char letter;
-
-	for (letter 'a'; letter <= 'z'; letter++)
-		putchar(letter);
-
-	for (letter 'A'; letter <= 'Z'; letter++)
-		putchar(letter);
-
+	for (c = 48; c <= 57; c++)
+	{
+		putchar(c);
+		if (c != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
 	putchar('\n');
-
 	return (0);
 }
-
